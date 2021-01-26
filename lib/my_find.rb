61e76f2c0310f037841ea884f78435ead6657629
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-26 01:38:03 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-i = 0
-empty = [ ]
-while i < collection.length 
-if yeild (collection[i])
-  return collection[i]
-i = i + 1
-end
-end
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
